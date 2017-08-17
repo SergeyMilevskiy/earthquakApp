2e42f6be11ceb6d8a01bdfca7623704c720db198
Sergey Milevskiy <sergey.milevskiy@xtb.com>
2017-08-17 14:26:14 +0200

    retrolambda dagger2

---
+++ app/src/main/java/com/sergey/earthqukeapp/view/MainFragmentList.java
@@ -0,0 +1,15 @@
+package com.sergey.earthqukeapp.view;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by smilevkiy on 17.08.17.
+ */
+
+public class MainFragmentList extends Fragment {
+
+
+
+
+
+}
